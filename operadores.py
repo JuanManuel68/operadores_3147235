07cd57ce7c784e93e2ef2216101d945563b69e98
faiber <zabalad789@gmail.com>
2025-02-28 09:45:00 -0500

    parentesis

--- operadores.py
+++ operadores.py
@@ -1,14 +1,19 @@
 '''
 JERARQUIA DE SIMBOLOS DE PYTOHN 
-1. /, *, %
-2. +, -
-3. =
+1.        (  )
+2.         ** 
+3.      /, *, %
+4.        +, -
+5.          =
 
-si hay operaciones en el mismo nivel de jerarquia, se resuelven de izquierda a derecha
+-si hay operaciones en el mismo nivel de jerarquia, se resuelven de izquierda a derecha
+-si hay parentesis dentro del parentesis se resuelve el parentesis interno
 '''
 
 a = 3
 b = 2 
 c = 1
-y = a**2 - a * b - 1 / 3 + c
+x=5
+y = ((2*a+c)/7)*(a+(4*a)/c)
 print(y)  # Salida: 7
+
