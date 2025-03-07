6d04c5464d11e2f55e8389ae4c4e63afa289e8a2
faiber <zabalad789@gmail.com>
2025-03-07 06:40:00 -0500

    guardado

--- operadores.py
+++ operadores.py
@@ -14,6 +14,28 @@ a = 3
 b = 2 
 c = 1
 x=5
-y = ((2*a+c)/7)*(a+(4*a)/c)
+y = c/(c+2)<c*a-c+1-b*2
 print(y)  # Salida: 7
 
+''' 
+OPERADORES RELACIONALES 
+Las operaciones aritmeticas resultan en un valos numerico:
+LAS OPERACIONES RELACIONALES 
+Resultan en un valor booleano:
+True o False(v, f, si o no)
+>,<,>=,<=,!=,==
+JERARQUIA DE OPERADORES 
+(Incluyendo los relacionales)
+1.        (  )
+2.         **
+3.      /, *, %
+4.        +, -
+5.  <, >, <=, >=, !=, ==
+6.        =
+'''
+a = 2
+b = 3 
+c = 7
+x=5
+y=c/(x+2)<c*a-c+1-b*2
+print(y)  # Salida
